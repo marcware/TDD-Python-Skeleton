cfb8ddd0a77a790a26a00883e3e4aa46eed1b2e8
MarcosPalacios <fmpalacios@alea-soluciones.com>
2023-06-26 23:26:02 +0200

    Kata Kamel case

--- src/ab_camel_case.py
+++ src/ab_camel_case.py
@@ -1,3 +1,13 @@
+import re
+
+
 class CamelCase:
-    def convert_string(self, data: str) -> str:
-        return data.strip().replace(" ", "")
+    def convert_string(self, data_to_convert: str) -> str:
+        regex = r"[-_ ]"
+        data_capitalize = data_to_convert.title()
+        return re.sub(regex, '', data_capitalize)
+
+
+if __name__ == '__main__':
+    camel_case = CamelCase()
+    response = camel_case.convert_string('foo_bar')
--- tests/ab_camel_case_test.py
+++ tests/ab_camel_case_test.py
@@ -15,3 +15,20 @@ def test_send_Foo_Bar_return_FooBar():
     camel_case = CamelCase()
     response = camel_case.convert_string('Foo Bar')
     assert response == 'FooBar'
+
+
+def test_send_Foo_BarFoo_return_FooBar():
+    camel_case = CamelCase()
+    response = camel_case.convert_string('Foo_Bar-Foo')
+    assert response == 'FooBarFoo'
+
+
+def test_converts_the_first_character_of_one_word_to_uppercase():
+    camel_case = CamelCase()
+    response = camel_case.convert_string('foo')
+    assert response == 'Foo'
+
+def test_converts_the_first_character_of_each_word_to_uppercase():
+    camel_case = CamelCase()
+    response = camel_case.convert_string('foo_bar')
+    assert response == 'FooBar'
